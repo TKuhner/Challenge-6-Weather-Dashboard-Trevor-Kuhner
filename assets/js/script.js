84df241eecc972a22e5a9197d7cd81d330fc2d2e
TKuhner <trevorkuhner29@gmail.com>
2023-05-29 18:27:53 -0500

    completed initial html layout and css formatting

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+//call weather api
+//call location api
+// get location
+// get weather
+//get search inquiry
+//get search history
+//store search inquiry into local storage
+//get 5 day forecast
+//search button
+//previous searches are clickable
+
+//global variables
+const searchBtn = document.querySelector("#searchBtn");
+let searchInput = document.querySelector("#searchInput");
